204153b2d3950ba01571334bc1637aa73ecaa41a
Ubuntu <ubuntu@ip-172-31-7-129.ap-northeast-1.compute.internal>
2026-05-11 13:48:44 +0000

    認証の「formデータを受け取る」所まで書いた

--- todolist/app/Http/Controllers/AuthController.php
+++ todolist/app/Http/Controllers/AuthController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginPostRequest;
 
 class AuthController extends Controller
 {
     public function index(){
         return view('index');
+    }
+
+    public function login(LoginPostRequest $request)
+    {
+        $datum = $request->validated();
+
+        var_dump($datum); exit;
     }
 }
--- todolist/resources/views/index.blade.php
+++ todolist/resources/views/index.blade.php
@@ -2,7 +2,15 @@
 
 @section('contents')
 <h1>ログイン</h1>
+@if ($errors->any())
+  <div>
+      @foreach ($errors->all() as $error)
+      {{ $error }}<br>
+      @endforeach
+  </div>
+@endif
   <form action="/login" method="post">
+    @csrf
     email: <input type="text" name="email"><br>
     password: <input type="password" name="password"><br>
     <button>ログインする</button>
